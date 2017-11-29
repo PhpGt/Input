3a6e25e2a0b103cfe492effaccb022d965485b45
Greg Bowler <greg.bowler@g105b.com>
2017-11-29 16:35:35 +0000

    Begin testing get/post

--- test/unit/InputTest.php
+++ test/unit/InputTest.php
@@ -17,4 +17,30 @@ class InputTest extends TestCase {
 		$body = $input->getStream();
 		self::assertEquals($testMessage, (string)$body);
 	}
+
+	/**
+	 * @dataProvider dataRandomGetPost
+	 */
+	public function testGetCombined(array $_GET, array $_POST):void {
+
+	}
+
+	public function dataRandomGetPost():array {
+		$data = [];
+
+		for($i = 0; $i < 100; $i++) {
+			$params = [$this->getRandomKvp(), $this->getRandomKvp()];
+			$data []= $params;
+		}
+
+		return $data;
+	}
+
+	private function getRandomKvp():array {
+		$kvp = [];
+
+
+
+		return $kvp;
+	}
 }
