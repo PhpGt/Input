609b87e133f96be07e7f7e21d550b7482165a815
Greg Bowler <greg.bowler@g105b.com>
2017-12-01 18:32:23 +0000

    Implement WithWithoutClashingException

--- src/InputDataFactory.php
+++ src/InputDataFactory.php
@@ -2,12 +2,23 @@
 namespace Gt\Input;
 
 class InputDataFactory {
-	public static function create(Input $input, array $with, array $without):InputData {
+	public static function create(Input $input, array $with = [], array $without = []):InputData {
 		$data = $input->getAll();
 
 		if(empty($with)
 		&& empty($without)) {
 			return $data;
+		}
+
+// It's fine to call $input->with()->without, but not if the same value exists in with and without.
+		if(!empty($with)
+		&& !empty($without)) {
+			$clash = array_intersect($with, $without);
+			if(!empty($clash)) {
+				throw new WithWithoutClashingException(
+					implode(", ", $clash)
+				);
+			}
 		}
 
 		if(!empty($without)) {
---
+++ src/WithWithoutClashingException.php
@@ -0,0 +1,4 @@
+<?php
+namespace Gt\Input;
+
+class WithWithoutClashingException extends InputException {}
