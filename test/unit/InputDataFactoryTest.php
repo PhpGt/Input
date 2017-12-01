88b042706a92ed5bd547ea9c083b0bb3fbf3f8df
Greg Bowler <greg.bowler@g105b.com>
2017-12-01 18:23:18 +0000

    Test create, no criteria

---
+++ test/unit/InputDataFactoryTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Gt\Input\Test;
+
+use Gt\Input\Input;
+use Gt\Input\InputDataFactory;
+use PHPUnit\Framework\TestCase;
+
+class InputDataFactoryTest extends TestCase {
+	public function testCreateNoCriteria():void {
+		$input = $this->createInput();
+		$data = InputDataFactory::create($input);
+		self::assertEquals("Edward", $data["name"]);
+		self::assertEquals(51, $data["age"]);
+		self::assertEquals("01234 567890", $data["telephone"]);
+		self::assertEquals("AB12 3CD", $data["postcode"]);
+	}
+
+	protected function createInput() {
+		return new Input([
+			"name" => "Edward",
+			"age" => 51,
+			"telephone" => "01234 567890",
+			"postcode" => "AB12 3CD",
+		]);
+	}
+}
