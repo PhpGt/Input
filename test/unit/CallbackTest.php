7d6373263cdf6ea6590a2a5bc209a772478a2a14
Greg Bowler <greg.bowler@g105b.com>
2017-12-01 15:40:40 +0000

    Test Callback class

---
+++ test/unit/CallbackTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Gt\Input\Test;
+
+use Gt\Input\Callback;
+use Gt\Input\InputData;
+use PHPUnit\Framework\TestCase;
+
+class CallbackTest extends TestCase {
+	public function testCallNoArgs() {
+		$callbackArgs = null;
+
+		$callback = new Callback(function(...$args) use(&$callbackArgs) {
+			$callbackArgs = $args;
+		});
+		$inputData = self::createMock(InputData::class);
+		$callback->call($inputData);
+
+		self::assertCount(1, $callbackArgs);
+		self::assertInstanceOf(InputData::class, $callbackArgs[0]);
+	}
+
+	public function testCallWithArgs() {
+		$callbackArgs = null;
+		$firstParam = "one";
+		$secondParam = "two";
+		$thirdParam = "three";
+
+		$callback = new Callback(function(...$args) use (&$callbackArgs) {
+			$callbackArgs = $args;
+		}, $firstParam, $secondParam, $thirdParam);
+		$inputData = self::createMock(InputData::class);
+		$callback->call($inputData);
+
+		self::assertCount(4, $callbackArgs);
+		self::assertEquals($firstParam, $callbackArgs[1]);
+		self::assertEquals($secondParam, $callbackArgs[2]);
+		self::assertEquals($thirdParam, $callbackArgs[3]);
+	}
+}
